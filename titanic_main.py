fba618991329cde0a660eb75b80f852f70858b23
John <johnwilles@gmail.com>
2017-03-15 22:33:03 -0400

    Add initial class and sex comparison analysis

--- titanic_main.py
+++ titanic_main.py
@@ -17,6 +17,36 @@ from sklearn.linear_model import Perceptron
 from sklearn.linear_model import SGDClassifier
 from sklearn.tree import DecisionTreeClassifier
 
-train_df = pd.read_csv('./train.csv')
+# Options
+pd.options.display.width = 200
+test_output = True
 
-print(train_df.columns.values)
+# Loading Data
+print('Loading data')
+train_df = pd.read_csv('./data/train.csv')
+test_df = pd.read_csv('./data/test.csv')
+combine = [train_df, test_df]
+
+# Testing
+if test_output:
+
+  print('Testing')
+
+  # Output df titles, shape, info and head
+  #print(train_df.columns.values)
+  #print('_'*40)
+  #print train_df.shape
+  #print('_'*40)
+  print(train_df.head())
+  #print('_'*40)
+  #print(train_df.info())
+  #print('_'*40)
+  #print(train_df.describe())
+  print('_'*40)
+
+  # Output Survival vs tables
+  print(train_df[['Pclass','Survived']].groupby(['Pclass'], as_index=False).mean().sort_values('Survived', ascending=False))
+  print('_'*40)
+  print(train_df[['Sex','Survived']].groupby(['Sex'], as_index=False).mean().sort_values('Survived', ascending=False))
+  print('_'*40)
+  print(train_df[['Embarked','Survived']].groupby(['Embarked'], as_index=False).mean().sort_values('Survived', ascending=False))
